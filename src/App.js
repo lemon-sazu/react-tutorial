0183c075e49a34fa35d4382891397ef770462614
lemon-sazu <lemon.sazu@gmail.com>
2022-12-05 23:27:25 +0600

    update

--- src/App.js
+++ src/App.js
@@ -2,26 +2,66 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const countIncrese = () => {
-    setCount(count+1);
+  const [newTask, setNewTask] = useState('');
+  const [tasklist, setTasklist] = useState([]);
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
   }
-  const countDecrese = () => {
-    setCount(count-1)
+  const addToTaskList = () => {
+    const task = {
+      id: tasklist.length === 0 ? 1 : tasklist.length + 1,
+      name: newTask,
+      complete: false
+    }
+    setTasklist([...tasklist, task]);
+    console.log(task)
   }
-  const countSetZero = () => {
-    setCount(0);
+  const deleteTask = (taskid) => {
+
+    const setNewTask =
+      tasklist.filter((task) => {
+        return task.id !== taskid
+      })
+
+    setTasklist(setNewTask)
+  }
+  const markAsDone = (id) => {
+    setTasklist(
+
+      tasklist.map((task, key) => {
+        if (task.id === id) {
+          return { ...task, complete: true }
+        } else {
+          return task;
+        }
+      })
+
+    )
   }
   return (
-    <div style={{ textAlign: "center" }}>
-      <h2>{count}</h2>
-      <div>
-        <button onClick={countIncrese}>Increase</button>
-        <button onClick={countDecrese}>Decrease</button>
-        <button onClick={countSetZero}>Set To Zero</button>
+    <div>
+      <div className='add_area'>
+        <div className="add_area-inputs">
+          <input onChange={handleChange} className='task_input' type="text" placeholder='Do Homework' />
+          <button onClick={addToTaskList} className='addtask_btn'>Add Task</button>
+        </div>
       </div>
+      {tasklist.map((task, key) => {
+        return <TaskCard name={task.name} markAsDone={markAsDone} complete={task.complete} key={key} id={task.id} deleteTask={deleteTask} />
+      })}
     </div >
   );
+}
+
+const TaskCard = (props) => {
+  return <div className="list_area" style={{ backgroundColor: props.complete ? 'lawngreen' : '' }}>
+    <div className="list_area-content">
+      <h4>{props.name}</h4>
+      {props.complete ? 'Done' : <button className='complete_btn' onClick={() => props.markAsDone(props.id)}>Mark As Done</button>}
+      <button onClick={() => props.deleteTask(props.id)} className='close_btn'>x</button>
+    </div>
+
+  </div>
 }
 
 export default App;
