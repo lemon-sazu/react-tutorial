25afd7b65268061d34d5eefe89ef4ef8da718f86
lemon-sazu <lemon.sazu@gmail.com>
2022-12-05 11:10:01 +0600

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { planets } from './Planets'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {planets.map((planet, key)=>{
+        return planet.isGasPlanet? <PlanetCart key={key} name={planet.name} /> :''
+      })}
     </div>
   );
+}
+
+const PlanetCart = (prop) => {
+  return (
+    <div>
+      <h2>{prop.name}</h2>
+    </div>
+  )
 }
 
 export default App;
