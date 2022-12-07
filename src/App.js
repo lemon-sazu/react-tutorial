8b54f6c11eca7f8148d5ad4e46ba4a5eeb9f7dda
lemon-sazu <lemon.sazu@gmail.com>
2022-12-07 23:28:16 +0600

    Updated on Branch moduleFour

--- src/App.js
+++ src/App.js
@@ -1,67 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import Axios from 'axios'
 function App() {
-  const [newTask, setNewTask] = useState('');
-  const [tasklist, setTasklist] = useState([]);
+
+  const [name, setName] = useState('');
+  const [predict, setPredict] = useState();
   const handleChange = (event) => {
-    setNewTask(event.target.value)
+    setName(event.target.value)
   }
-  const addToTaskList = () => {
-    const task = {
-      id: tasklist.length === 0 ? 1 : tasklist.length + 1,
-      name: newTask,
-      complete: false
-    }
-    setTasklist([...tasklist, task]);
-    console.log(task)
+
+  // useEffect(() => {
+  //   getPredict();
+  // }, [])
+
+  const getPredict = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredict(res.data);
+      console.log(predict);
+    })
   }
-  const deleteTask = (taskid) => {
 
-    const setNewTask =
-      tasklist.filter((task) => {
-        return task.id !== taskid
-      })
-
-    setTasklist(setNewTask)
-  }
-  const markAsDone = (id) => {
-    setTasklist(
-
-      tasklist.map((task, key) => {
-        if (task.id === id) {
-          return { ...task, complete: true }
-        } else {
-          return task;
-        }
-      })
-
-    )
-  }
   return (
     <div>
       <div className='add_area'>
         <div className="add_area-inputs">
-          <input onChange={handleChange} className='task_input' type="text" placeholder='Do Homework' />
-          <button onClick={addToTaskList} className='addtask_btn'>Add Task</button>
+          <input type="text" onChange={handleChange} className='task_input' placeholder='Your Name' />
+          <button onClick={getPredict} className="addtask_btn">Submit</button>
         </div>
       </div>
-      {tasklist.map((task, key) => {
-        return <TaskCard name={task.name} markAsDone={markAsDone} complete={task.complete} key={key} id={task.id} deleteTask={deleteTask} />
-      })}
+      <div className="content_area">
+        <h2>Age Predictor</h2>
+        <p><b>Name: </b> {predict?.name} </p>
+        <p><b>Age: </b> {predict?.age} </p>
+      </div>
     </div >
   );
 }
 
-const TaskCard = (props) => {
-  return <div className="list_area" style={{ backgroundColor: props.complete ? 'lawngreen' : '' }}>
-    <div className="list_area-content">
-      <h4>{props.name}</h4>
-      {props.complete ? 'Done' : <button className='complete_btn' onClick={() => props.markAsDone(props.id)}>Mark As Done</button>}
-      <button onClick={() => props.deleteTask(props.id)} className='close_btn'>x</button>
-    </div>
 
-  </div>
-}
 
 export default App;
